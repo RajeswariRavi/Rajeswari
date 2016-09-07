cd47d00bf7cd57a54a3f8b6dc93171743c63fecc
RajeswariRavi <rajieeect@gmail.com>
2016-09-07 11:10:50 +0530

    Add files via upload

---
+++ Conversion.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Conversion
+{
+public static void main(String[] arg)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the string:");
+String s=sc.nextLine();
+int n=Integer.valueOf(s);
+System.out.println(n);
+}
+}
